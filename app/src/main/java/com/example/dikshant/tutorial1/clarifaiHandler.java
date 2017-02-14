307d3698e15975a0f3208d2744f397935d3b557c
Dikshant <dpradha2@illinois.edu>
2017-02-14 14:39:49 -0600

    adding in clarifai

---
+++ app/src/main/java/com/example/dikshant/tutorial1/clarifaiHandler.java
@@ -0,0 +1,20 @@
+package com.example.dikshant.tutorial1;
+
+/**
+ * Created by Dikshant on 2/14/2017.
+ */
+
+import android.support.annotation.Nullable;
+
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+//import okhttp3.OkHttpClient;
+//import okhttp3.logging.HttpLoggingInterceptor;
+
+
+public class clarifaiHandler {
+
+    @Nullable
+    private ClarifaiClient client;
+
+}
