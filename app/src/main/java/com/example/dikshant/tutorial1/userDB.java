1734b07105a8cc3a45c1d4f30e31aa6b733b9040
Dikshant <dikshantpradhan@gmail,com>
2017-03-29 00:03:55 -0500

    Updating Layout and Fonts (3)

--- app/src/main/java/com/example/dikshant/tutorial1/userDB.java
+++ app/src/main/java/com/example/dikshant/tutorial1/userDB.java
@@ -64,5 +64,53 @@ public class userDB extends DBHandler {
         db.execSQL("delete from "+ TABLE_NAME);
     }
 
+    public Cursor queryContainingFood(String word){
+        Log.d("nDB", "querying");
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME, new String[] {KEY_DATE, KEY_FOOD, KEY_CARBS},
+                KEY_FOOD + " LIKE ?", new String[] {"%" + word + "%"},
+                null, null, null);
+
+        Log.d("nDB", String.valueOf(cursor.getCount()));
+
+        //Log.d("nDB query", cursor.getString(cursor.getColumnIndex(DESC)));
+
+        return cursor;
+    }
+
+    public Cursor queryCarb(String word){
+        Log.d("nDB", "querying");
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME, new String[] {KEY_DATE, KEY_FOOD, KEY_CARBS},
+                KEY_CARBS + " LIKE ?", new String[] {"%" + word + "%"},
+                null, null, null);
+
+        Log.d("nDB", String.valueOf(cursor.getCount()));
+
+        //Log.d("nDB query", cursor.getString(cursor.getColumnIndex(DESC)));
+
+        return cursor;
+    }
+
+    public Cursor queryDate(String word){ // need to change this one
+        Log.d("nDB", "querying");
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME, new String[] {KEY_DATE, KEY_FOOD, KEY_CARBS},
+                "*", new String[] {"%" + word + "%"},
+                null, null, null);
+
+        Log.d("nDB", String.valueOf(cursor.getCount()));
+
+        //Log.d("nDB query", cursor.getString(cursor.getColumnIndex(DESC)));
+
+        return cursor;
+    }
+
     // need reading functions and export and others???
 }
