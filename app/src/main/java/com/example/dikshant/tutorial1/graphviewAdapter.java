07d9915377c05ab919bf11b539928b2b8898163b
Dikshant <dikshantpradhan@gmail,com>
2017-04-13 22:29:48 -0500

    updating graphing

--- app/src/main/java/com/example/dikshant/tutorial1/graphviewAdapter.java
+++ app/src/main/java/com/example/dikshant/tutorial1/graphviewAdapter.java
@@ -2,6 +2,7 @@ package com.example.dikshant.tutorial1;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
@@ -41,23 +42,34 @@ public class graphviewAdapter {
         DataPoint[] values = new DataPoint[length];
 
         for (int i = 0; i < length; i++){
-            values[i] = new DataPoint(X.get(i), Y.get(i));
+            values[i] = new DataPoint(X.get(i).getDate(), Y.get(i));
         }
 
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(values);
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(values);
         graph.addSeries(series);
 
         // set date label formatter
         //graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getActivity()));
-        graph.getGridLabelRenderer().setNumHorizontalLabels(length); // only 4 because of the space
+        //graph.getGridLabelRenderer().setNumHorizontalLabels(length); // only 4 because of the space
 
 // set manual x bounds to have nice steps
-        graph.getViewport().setMinX(X.get(0).getDate());
-        graph.getViewport().setMaxX(X.get(length - 1).getDate());
-        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(X.get(0).getDay());
+        graph.getViewport().setMaxX(X.get(length - 1).getDay());
+        //graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(500);
+        //graph.getViewport().setYAxisBoundsManual(true);
+
+        //String[] stockArr = new String[X.size()];
+        //stockArr = X.toArray(stockArr);
+        //graph.getGridLabelRenderer().
+
 
 // as we use dates as labels, the human rounding to nice readable numbers
 // is not necessary
         graph.getGridLabelRenderer().setHumanRounding(false);
+        graph.getGridLabelRenderer().setHorizontalAxisTitle("Date (April)");
+        graph.getGridLabelRenderer().setVerticalAxisTitle("Carbohydrates Consumed per Day");
+        graph.setTitle("Carbohydrate Consumption History");
     }
 }
